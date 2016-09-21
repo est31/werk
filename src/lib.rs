d8e5f1f4df5bb774dbb014c9261c8334df2d5da7
est31 <MTest31@outlook.com>
2016-09-21 18:13:16 +0200

    Add license and basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+// Opus decoder written in Rust
+//
+// Copyright (c) 2016 the contributors.
+// Licensed under MIT license, or Apache 2 license,
+// at your option. Please see the LICENSE file
+// attached to this source distribution for details.
+
+#![deny(unsafe_code)]
+
+/*!
+An `opus` decoder, written in Rust.
+
+This crate implements an `opus` decoder. opus is specified in
+[RFC 6716](https://tools.ietf.org/html/rfc6716).
+
+Currently this is in the early stages of development,
+nothing actually works yet.
+*/
