6f60f59c468defdb1cc6087e60b6ee862e54efb9
est31 <MTest31@outlook.com>
2017-07-11 06:39:16 +0200

    Add a run_tests.sh script that runs tests
    Just cd'ing into werk_tests is not possible because of linker errors.
    Probably a result of the hack of the previous commit.

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Werk - a pure Rust opus library
+#
+# Copyright (c) 2001-2011 the opus developers, and
+# Copyright (c) 2017 est31 <MTest31@outlook.com>
+# and contributors, All rights reserved.
+# Licensed under the BSD 3 clause license.
+# Please see the COPYING file attached to
+# this source distribution for details.
+
+cur_dir=`dirname "$0"`
+cd "$cur_dir/werk_test"
+
+cargo run --release --bin test_opus_decode
+cargo run --release --bin test_opus_encode
