f6e84f6ac9d50e603a9bab8af31113d08390f943
est31 <MTest31@outlook.com>
2017-07-12 04:25:38 +0200

    Replace test_program! with program!

--- werk_test/build.rs
+++ werk_test/build.rs
@@ -11,7 +11,7 @@ extern crate gcc;
 
 use gcc::Config;
 
-macro_rules! test_program {
+macro_rules! program {
 	($($($pathseg:ident)/ *.c),*; $libname:expr) => {{
 		let mut cfg = Config::new();
 		cfg
@@ -37,65 +37,65 @@ macro_rules! test_program {
 }
 
 fn celt_tests() {
-	test_program! {
+	program! {
 		celt/tests/test_unit_cwrs32.c;
 		"libcelt_cwrs.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_dft.c;
 		"libcelt_dft.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_entropy.c;
 		"libcelt_entropy.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_laplace.c;
 		"libcelt_laplace.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_mathops.c;
 		"libcelt_mathops.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_mdct.c;
 		"libcelt_mdct.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_rotation.c;
 		"libcelt_rotation.a"
 	}
-	test_program! {
+	program! {
 		celt/tests/test_unit_types.c;
 		"libcelt_types.a"
 	}
 }
 
 fn silk_tests() {
-	test_program! {
+	program! {
 		silk/tests/test_unit_LPC_inv_pred_gain.c;
 		"libsilk_inv_pred_gain.a"
 	}
 }
 
 fn opus_tests() {
-	test_program! {
+	program! {
 		tests/test_opus_decode.c;
 		"libopus_decode.a"
 	}
 
-	test_program! {
+	program! {
 		tests/test_opus_encode.c,
 		tests/opus_encode_regressions.c;
 		"libopus_encode.a"
 	}
 
-	test_program! {
+	program! {
 		tests/test_opus_api.c;
 		"libopus_api.a"
 	}
 
-	test_program! {
+	program! {
 		tests/test_opus_padding.c;
 		"libopus_padding.a"
 	}
